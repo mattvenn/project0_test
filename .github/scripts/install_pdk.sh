b612dca6712d85804800b6adaafb1c45a59c8a8e
matt venn <matt@mattvenn.net>
2021-12-27 22:25:00 +0100

    update

--- .github/scripts/install_pdk.sh
+++ .github/scripts/install_pdk.sh
@@ -3,8 +3,6 @@ git clone https://github.com/efabless/caravel_user_project.git
 cd caravel_user_project
 git checkout mpw-3
 
-export CARAVEL_ROOT=$(pwd)/caravel
-
 make install
 cd caravel
 git checkout $CARAVEL_COMMIT
